4a655a96d0a33aef060594205782a170bbe64c6e
Marcos Kim <marcos.skim@senacsp.edu.br>
2024-11-14 10:18:53 -0300

    correção dos exercícios de python

--- exerc_funcoes1.py
+++ exerc_funcoes1.py
@@ -8,26 +8,31 @@ def celsius_fahrenheit(c):
    c = float(c)
    f = float((9 * c) / 5) + 32
 
-   return ('A temperatura em fahrenheit: {0}°F'.format(f))
+  # return 'A temperatura em fahrenheit: {0}°F'.format(f)
+   return "A temperatura em Fahrenheit é: "+str(f)+"°F" # + significa concatenação
 
 
 # Exercício 3:
 def par_impar (n):
-    resto = n % 2
-    if resto == 0:
+    if n % 2 == 0:
         print("O número é par.")
     else:
         print("O número é ímpar.")
 
 
 # Exercício 4:
-def string_invertida (string):
-    string_invertida = ''.join(reversed(string))
-    print(string_invertida)
-   
-
+def string_invertida (texto):
+    # string_invertida = ''.join(reversed(texto))
+    # print(string_invertida)
+    tamanho_do_texto = len(texto)
+    for i in range(tamanho_do_texto-1,-1,-1): # o primeiro -1 é para indicar em qual posição devemos começar,
+        # o segundo -1 é referente à posição e o terceiro -1 é referente à quantidade de casas a andar.
+        print(texto[i],end="") # end="" para imprimir horizontalmente.
+    print()
+    
 # Exercício 5:
 def verificar_palindromo(palavra):
+    palavra = palavra.lower()
     palavra_invertida = palavra[::-1]
     if palavra == palavra_invertida:
         return ("A palavra é um palíndromo.")
@@ -42,12 +47,14 @@ def media (*numeros):
     media = soma / len(*numeros)
     print("A média dos números é:", media)
 
+# def media (lista_numeros):
+
 
 # Exercício 7:
-def pares (lista1):
-    lista1 = []
-    lista2 = []
+def pares (lista_numeros):
+    lista_pares = []
 
-    for lista2 in lista1:
-        if lista1 % 2 == 0:
-            print(lista2)
+    for i in lista_numeros:
+        if i % 2 == 0:
+            lista_pares.append(i)
+    return lista_pares
